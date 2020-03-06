ea99726fd6ff76ebff9f2122872ece367e6c48b9
delightedwaif <tim@milkmoovement.com>
2020-03-06 11:38:29 -0330

    fixing history

--- assign3/shell.c
+++ assign3/shell.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "shell.h"
+#include <errno.h>
 
 /*
 Timothy Mather shell
@@ -33,7 +34,7 @@ char **get_args(char *buffer)
     char *token = strtok(buffer, delims);
     char **args = (char *)calloc(20, sizeof(char *));
     args[0] = token;
-    int i = 1;
+    int j = 1;
     int file_next = 0;
     while (token != NULL)
     {
@@ -59,9 +60,9 @@ char **get_args(char *buffer)
         }
         else
         {
-            args[i] = token;
+            args[j] = token;
         }
-        i++;
+        j++;
     }
     return args;
 }
@@ -74,85 +75,93 @@ int main(int argc, char *argv[])
     // allocate arguments and history space
     int buff_size = 64;
     char **args;
-    char *buff = (char *)malloc(buff_size * sizeof(char));
     char **history = (char *)calloc(100, sizeof(char *));
-
     // set up signal handling
     signal(SIGINT, signal_handler);
     signal(SIGTSTP, signal_handler);
 
     while (1)
     {
+        char *buff = (char *)malloc(buff_size * sizeof(char));
         printf("$ ");
         size_t c = getline(&buff, &buff_size, stdin);
         if (c == 1) // this prevents errors by executing child with no arguments
         {
             continue;
         }
-        args = get_args(buff);
-
-        strcpy(history + i, args[0]); // store argument in history
-        pid_t pid = fork();
-        if (pid == 0)
+        else if (c == -1)
         {
-            children = 0;
-            // execute child
-            int fd;
-            // redirect outputs to file if requested
-            if (fileredir == 1)
+            printf("%ld\n", stdin);
+            perror("getline");
+            continue;
+        }
+        strcpy(history + i, buff); // store argument in history
+        // handle printing history
+
+        if (strcmp(history + i, "history\n") == 0)
+        {
+            printf("Session History: \n");
+            for (int j = 0; j < i + 1; j++)
             {
-                fd = open(filename,  O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-                dup2(fd, 1);
+                printf("%s", (history + j));
             }
-            if (err_redir == 1)
-            {
-                fd = open(filename,  O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-                dup2(fd, 2);
-            }
-            close(fd);
-            // execute arguments
-            execvp(args[0], args);
         }
         else
         {
-            // handle printing history
-            if (strcmp(history + i, "history") == 0)
+            args = get_args(buff);
+
+            pid_t pid = fork();
+            if (pid == 0)
             {
-                printf("Session History: \n");
-                for (int j = 0; j < i + 1; j++)
+                children = 0;
+                // execute child
+                int fd;
+                // redirect outputs to file if requested
+                if (fileredir == 1)
                 {
-                    printf("%s\n", (history + j));
+                    fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+                    dup2(fd, 1);
                 }
-                continue;
-            }
-            children += 1;
-            int stat;
-            char buffer[4096];
-            pid_t w = waitpid(pid, &stat, WUNTRACED);
-            children -= 1;
-            fileredir = 0;
-            err_redir = 0;
-            if (WIFEXITED(stat))
-            {
-                printf("Exited with status: %d\n", WEXITSTATUS(stat));
-            }
-            else if (WTERMSIG(stat) != 127)
-            {
-                printf("\nTerminated by %s\n", strsignal(WTERMSIG(stat)));
+                if (err_redir == 1)
+                {
+                    fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+                    dup2(fd, 2);
+                }
+                if (fd)
+                {
+                    close(fd);
+                }
+                // execute arguments
+                execvp(args[0], args);
             }
             else
             {
-                printf("Suspended\n");
+                children += 1;
+                int stat;
+                char buffer[4096];
+                pid_t w = waitpid(pid, &stat, WUNTRACED);
+                children -= 1;
+                fileredir = 0;
+                err_redir = 0;
+                if (WIFEXITED(stat))
+                {
+                    printf("Exited with status: %d\n", WEXITSTATUS(stat));
+                }
+                else if (WTERMSIG(stat) != 127)
+                {
+                    printf("\nTerminated by %s\n", strsignal(WTERMSIG(stat)));
+                }
+                else
+                {
+                    printf("Suspended\n");
+                }
             }
+            i++;
+            free(buff);
         }
-        i++;
     }
-    free(buff);
-    free(args);
-    free(history);
     return 0;
 }
-
 
 // signal handling
 void signal_handler(int sig)
